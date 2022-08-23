9c131079468bdfa3b72d4af749192895a5a82e93
ricardo <rosastaricardo84@gmail.com>
2022-08-23 06:45:29 -0300

    cod vers1

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-const form = document.querySelector("form");
+const div = document.createElement("div");
+
+const botao = document.querySelector("button");
+const divRow = document.querySelector(".row");
 
 const titulo = document.querySelector("#titulo");
 const descricao = document.querySelector("#descricao");
 const url = document.querySelector("#url");
 
-const listaInfoCard = [];
+const imagemForm = document.querySelector("#imagemForm");
+const tituloForm = document.querySelector("#tituloForm");
+const descricaoForm = document.querySelector("#descricaoForm");
+
 const infoCard = {};
 
-form.addEventListener("click", (event) => {
+const listaCards = [];
+
+function listaInfoCards() {
+  listaCards.push({
+    titulo: titulo.value,
+    descricao: descricao.value,
+    url: url.value,
+  });
+}
+
+function montaCard(obj) {
+  obj["titulo"] = titulo.value;
+  obj["descricao"] = descricao.value;
+  obj["url"] = url.value;
+
+  return `<img id="imagemForm" src="${obj.url}" alt=""><h2 id="tituloForm">${obj.titulo}</h2><p id="descricaoForm">${obj.descricao}</p>`;
+}
+
+const attributes = {
+  id: "card",
+  class: "col-12 col-sm-8",
+};
+
+function setAttributes(element, atributos) {
+  Object.keys(atributos).forEach((attr) => {
+    element.setAttribute(attr, atributos[attr]);
+  });
+}
+
+function adicionarInfoCard(event) {
   event.preventDefault();
 
-  infoCard["titulo"] = titulo.value;
-  infoCard["descricao"] = descricao.value;
-  infoCard["url"] = url.value;
-});
+  listaInfoCards();
 
-listaInfoCard.push(infoCard);
-console.log(listaInfoCard);
+  const elemento = montaCard(infoCard);
+
+  div.innerHTML = elemento;
+
+  setAttributes(div, attributes);
+
+  divRow.appendChild(div);
+
+  divRow.insertAdjacentElement("beforeend", div);
+}
+
+botao.addEventListener("click", adicionarInfoCard);
